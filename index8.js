2f5fffc69b8cc31cab1f9675ae128dab92c603f6
HosseinKolivand128 <ac.hossein.ac@gmail.com>
2023-11-10 12:53:11 +0330

    ArrayIterationMethods

---
+++ index8.js
@@ -0,0 +1,59 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const names = ["Ali", "Bahare", "Leyla", "Asma", "Fatemeh", "Mahsa", "Solmaz", "Maedeh", "Mahjan"];
+
+// for (let i = 0; i < array.length; i++) {
+// console.log(numbers[i]);
+// }
+
+/**@todo forOf */
+// for (const number of numbers) {
+//     console.log(Math.pow(number, 2));
+//     // console.log(number * 2);
+// }
+
+/**@todo forIn  : Returns array or obj keys and indexes*/
+// numbers.AC='Hossein';
+// for (const key in numbers) {
+//     console.log(key);
+// }
+
+/**@todo forEach */
+//Returns Undefined
+// const animals = ["Cat", "dog", "elephant"];
+// animals.forEach((animal) => {
+//     // console.log(animal);
+//     // animal = "A";
+// })
+
+/**@todo map */
+//Returns an Array
+// names.map((users) => { console.log(users) })
+// let numberList = numbers.map((number) => Math.pow(number, 2));
+// console.log(numberList);
+
+/**@todo map */
+
+// console.log(animals);
+
+//Regular function
+// function name(a, b) {
+//     return a * b;
+// }
+// console.log(name(5, 5));
+
+//Arrow function
+// const calculate = (a, b) => a * b;
+// console.log(calculate(9, 9));
+
+// //Example:
+// const greeting = (name) => {
+//     console.log(`Hey ${name}!`);
+// }
+
+// // function greeting(name) {
+// //     console.log(`Hey ${name}!`);
+// // }
+
+// greeting("fatemeh")
+
+
