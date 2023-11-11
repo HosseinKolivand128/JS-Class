8a1c892a08c3c11d86734377309b12f058545af9
MobinaNokhostin <mobina.nokhostin.12@gmail.com>
2023-11-12 03:03:29 +0330

    Q1 done

--- Q1.js
+++ Q1.js
@@ -26,3 +26,93 @@ Throw       Score
  */
 
 /** @todo WRITE YOUR CODES BELOW THIS */
+let dice1 = Math.floor(Math.random()*6)+1;
+let dice2 = Math.floor(Math.random()*6)+1;
+let dice3 = Math.floor(Math.random()*6)+1;
+let dice4 = Math.floor(Math.random()*6)+1;
+let dice5 = Math.floor(Math.random()*6)+1;
+const some = dice1.toString() + dice2.toString() + dice3.toString() +dice4.toString() + dice5.toString();
+
+console.log(`Throw :${some}`);
+
+function newScore(some) {
+    
+//1
+let first = some.replaceAll('1', '');
+let score1 = []
+if (first.length === 1) {
+    score1.push(1100);
+}else if (first.length === 2){
+    score1.push(1000);
+}else if (first.length === 3){
+    score1.push(200);
+}else if (first.length === 4){
+    score1.push(100);
+}else if (first.length === 5){
+    score1.push(0);
+}else{
+    score1.push(1200);
+}
+
+
+//6
+let score2 = []
+let second = some.replaceAll('6', '');
+if (second.length === 2) {
+    score2.push(600);
+}else{
+    score2.push(0);
+}
+
+
+//5
+let score3 = []
+let third = some.replaceAll('5', '');
+if (third.length === 1) {
+    score3.push(550);
+}else if (third.length === 2){
+    score3.push(500);
+}else if (third.length === 3){
+    score3.push(100);
+}else if (third.length === 4){
+    score3.push(50);
+}else if (third.length === 5){
+    score3.push(0);
+}else{
+    score3.push(600);
+}
+
+
+//4
+let score4 = []
+let fourth = some.replaceAll('4', '');
+if (fourth.length === 2) {
+    score4.push(400);
+}else{
+    score4.push(0);
+}
+
+//3
+let score5 = []
+let fifth = some.replaceAll('3', '');
+if (fifth.length === 2) {
+    score5.push(300);
+}else{
+    score5.push(0);
+}
+
+
+//2
+let score6 = []
+let sixth = some.replaceAll('2', '');
+if (sixth.length === 2) {
+    score6.push(200);
+}else{
+    score6.push(0);
+}
+
+const sum = parseInt(score1.toString()) + parseInt(score2.toString()) + parseInt(score3.toString()) + parseInt(score4.toString()) + parseInt(score5.toString()) + parseInt(score6.toString());
+return sum;
+}
+
+console.log(`Score :${newScore(some)}`);
